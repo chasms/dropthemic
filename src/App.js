88eadbba136b6ea2045b490f59646e9e65458203
NickAnaya618 <anaya.nicholas@gmail.com>
2017-04-07 16:45:59 -0400

    track rendered on page, yo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios'
-
+import config from '../.env.js'
 
 class App extends Component {
   constructor() {
@@ -10,57 +10,57 @@ class App extends Component {
       songs: []
     }
 
-    this.onVolumeChange = this.onVolumeChange.bind(this)
+    // this.onVolumeChange = this.onVolumeChange.bind(this)
   }
 
-  onVolumeChange(event) {
-    setState({
-      volume: {event.target.value}
-    })
-  }
+  // onVolumeChange(event) {
+  //   setState({
+  //     volume: {event.target.value}
+  //   })
+  // }
 
   componentWillMount(){
   axios
     .get(`https://freemusicarchive.org/api/get/tracks.json?api_key=${config.MY_KEY}`)
     .then(({data}) => {
-      this.setState({ songs: data})
+      this.setState({ songs: data.dataset})
     })
   }
 
   render() {
     return (
       <div className="container">
-        <Songs
+        <SongList
           songs={this.state.songs}
         />
       </div>
     )
   }
+}
 
-  const Songs = (props) => {
+  const SongList = (props) => {
     return (
       <div className="col-xs-4 songs">
-        <Songlist
-          songs={props.songs}
-        />
+        {props.songs.map(song => <SongListItem
+          key={song.track_id}
+          song={song}
+          />)
+        }
       </div>
     )
   }
 
-  const SongList = (props) => {
+  const SongListItem = (props) => {
     return (
-      <ul>
-        {props.songs.map((song) => <SongListItem
-          key={song.id}
-          trackFile={song.songFile}
-          songTitle={song.songTitle}
-          songArtist={song.songArtist}
-        />)
-        }
-      </ul>
+      <div id={props.song.track_id}>
+        <h5>{props.song.track_title} by {props.song.artist_name}</h5>
+        <audio controls>
+          <source src={"http://files.freemusicarchive.org/" + props.song.track_file} type="audio/mpeg"/>
+        </audio>
+      </div>
     )
   }
 
 
-}
+
 export default App;
